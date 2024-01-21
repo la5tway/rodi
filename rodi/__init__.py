914856b35748a464e11b7aca5bf086e91de535b5
Sergey Malanin <sergey.malanin@tmhsmart.ru>
2024-01-21 14:33:15 +0300

    added imports for backward compatibility

--- rodi/__init__.py
+++ rodi/__init__.py
@@ -1,7 +1,27 @@
 from .container import Container
 from .context import ActivationScope
+from .exceptions import (
+    AliasAlreadyDefined,
+    AliasConfigurationError,
+    CannotResolveParameterException,
+    CannotResolveTypeException,
+    DIException,
+    MissingTypeException,
+)
 from .inject import inject
 from .services import Services
 from .typings import ContainerProtocol
 
-__all__ = ("Container", "Services", "ActivationScope", "ContainerProtocol", "inject")
+__all__ = (
+    "Container",
+    "Services",
+    "ActivationScope",
+    "ContainerProtocol",
+    "inject",
+    "DIException",
+    "CannotResolveTypeException",
+    "CannotResolveParameterException",
+    "MissingTypeException",
+    "AliasAlreadyDefined",
+    "AliasConfigurationError",
+)
